cf4edd67698843ec157aed2a96006609b89f4ee4
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-18 14:26:06 +0000

    feature: Toggle style if todo is completed Add methods to delete and complete todo

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,10 +1,35 @@
 import React from 'react'
 
-export default function Todo({ task, completed }) {
+export default function Todo({ todo, completeTodo, deleteTodo }) {
+  const onComplete = (event, id) => {
+    completeTodo(id);
+  }
+
+  const onDelete = (event, id) => {
+    deleteTodo(id);
+  }
+
   return (
     <div>
-      <h5>{task}</h5>
-      <p>{completed}</p>
+      <div
+        style={
+          todo.completed
+          ? {textDecoration: 'line-through'}
+          : {textDecoration: ''}
+        }
+      >
+        <label>
+          <input
+            type="checkbox"
+            onClick={(event) => onComplete(event, todo.id)}
+          />
+            {todo.task}
+        </label>
+      </div>
+
+      <div
+        onClick={(event) => onDelete(event, todo.id)}
+      ><h5>X</h5></div>
     </div>
   )
 }
