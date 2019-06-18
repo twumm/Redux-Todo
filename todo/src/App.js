519248d3446be1a4c967c8b8822c652989633f50
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-18 12:27:04 +0000

    feature: Import combinedReducers Create redux store Pass store as prop to Provider

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { createStore ,combineReducers } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+import { combinedReducers } from './reducers/index';
 import Container from './components/Container';
 import './App.css';
 
+const store = createStore(
+  combinedReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
+
 function App() {
   return (
-    <Container />
+    <Provider store={store}>
+      <Container />
+    </Provider>
   );
 }
 
