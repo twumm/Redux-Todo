c90ec1c87fdded449f48437797aff9094d5bd1e0
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-18 14:23:57 +0000

    feature: Add cases for completing and deleting todo

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,15 +1,24 @@
 import { combineReducers } from 'redux';
 
-import { ADD_TODO } from '../actions/index';
+import { ADD_TODO, COMPLETE_TODO, DELETE_TODO } from '../actions/index';
 
 const initialState = {
   todos: [],
 }
 
-export function todosReducer(state = initialState, action) {
+export function todosReducer(state = initialState.todos, action) {
   switch (action.type) {
-    case ADD_TODO:
+    case (ADD_TODO):
       return [...state, action.payload]
+    case (COMPLETE_TODO):
+      return state.map(todo => {
+        if (todo.id === action.payload) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
+    case (DELETE_TODO):
+      return state.filter(todo => todo.id !== action.payload);
     default:
       return state
   }
