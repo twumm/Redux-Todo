01bace5bd66935574118dec81b8d6f421fa8266a
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-18 14:22:54 +0000

    feature: Add complete and delete todo

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,6 +1,8 @@
 import uuid from 'uuid';
 
 export const ADD_TODO = 'ADD_TODO';
+export const COMPLETE_TODO = 'COMPLETE_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
 
 export function addTodo(task) {
   return {
@@ -10,5 +12,19 @@ export function addTodo(task) {
       task,
       completed: false,
     }
+  }
+}
+
+export function completeTodo(id) {
+  return {
+    type: COMPLETE_TODO,
+    payload: id,
+  }
+}
+
+export function deleteTodo(id) {
+  return {
+    type: DELETE_TODO,
+    payload: id,
   }
 }
